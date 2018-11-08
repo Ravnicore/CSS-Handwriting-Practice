a42ac8239a01113cca5da8f28087fcdb05395b4d
Ravnicore <kevin.m.kelly@live.com>
2018-11-08 17:38:11 -0500

    Updates

---
+++ precache-manifest.5f209fcec76e95d2c7f908e17c9ae72b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c01982d9ae9f09aa78b0d3191221d5f",
+    "url": "/CSS-Handwriting-Practice/static/media/TinyTestVideo.2c01982d.mp4"
+  },
+  {
+    "revision": "69e855d490b308b924ae",
+    "url": "/CSS-Handwriting-Practice/static/js/runtime~main.69e855d4.js"
+  },
+  {
+    "revision": "bebc8cbdde3ff5f518b5",
+    "url": "/CSS-Handwriting-Practice/static/js/main.bebc8cbd.chunk.js"
+  },
+  {
+    "revision": "499bf8bcb9159cbc401f",
+    "url": "/CSS-Handwriting-Practice/static/js/1.499bf8bc.chunk.js"
+  },
+  {
+    "revision": "bebc8cbdde3ff5f518b5",
+    "url": "/CSS-Handwriting-Practice/static/css/main.b1ca54f6.chunk.css"
+  },
+  {
+    "revision": "518dd1053201e750aa72997849e5cae7",
+    "url": "/CSS-Handwriting-Practice/index.html"
+  }
+];
